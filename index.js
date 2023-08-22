8d08d8b3e440cca317cd692c5c4dff283cfba45e
alpa2 <134599717+alpa2@users.noreply.github.com>
2023-08-22 23:17:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-/*****************************************/
-/* Detect the MetaMask Ethereum provider */
-/*****************************************/
-
-// 直接使用 window.ethereum 获取钱包提供程序
 const provider = window.ethereum;
-
-
-
 
 if (provider) {
   startApp(provider);
@@ -14,66 +6,67 @@ if (provider) {
   console.log('Please install MetaMask!');
 }
 
-function startApp(provider) {
+async function startApp(provider) {
   if (provider !== window.ethereum) {
     console.error('Do you have multiple wallets installed?');
+    return;
   }
-}
 
-/**********************************************************/
-/* Handle chain (network) and chainChanged (per EIP-1193) */
-/**********************************************************/
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  const sendEthButton = document.querySelector('.sendEthButton');
+  const showAccount = document.querySelector('.showAccount');
 
-const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+  let accounts = [];
 
-window.ethereum.on('chainChanged', handleChainChanged);
+  // Send Ethereum to an address
+  sendEthButton.addEventListener('click', async () => {
+    try {
+      const transactionParameters = {
+        from: accounts[0], // The user's active address.
+        to: '0x9be00Ca9860D12244F2b56C2EdDB2F26e858EC92', // Replace with recipient address.
+        value: '100000000000000000', // Wei equivalent of 0.1 Ether.
+        gasLimit: '0x5028', // Customizable by the user during MetaMask confirmation.
+        maxPriorityFeePerGas: '0x3b9aca00', // Customizable by the user during MetaMask confirmation.
+        maxFeePerGas: '0x2540be400', // Customizable by the user during MetaMask confirmation.
+      };
 
-function handleChainChanged(chainId) {
-  window.location.reload();
-}
+      const response = await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
 
-/***********************************************************/
-/* Handle user accounts and accountsChanged (per EIP-1193) */
-/***********************************************************/
-
-let currentAccount = null;
-window.ethereum.request({ method: 'eth_accounts' })
-  .then(handleAccountsChanged)
-  .catch((err) => {
-    console.error(err);
+      console.log('Transaction response:', response);
+    } catch (error) {
+      console.error('Error sending transaction:', error);
+    }
   });
 
-window.ethereum.on('accountsChanged', handleAccountsChanged);
-
-function handleAccountsChanged(accounts) {
-  if (accounts.length === 0) {
-    console.log('Please connect to MetaMask.');
-  } else if (accounts[0] !== currentAccount) {
-    currentAccount = accounts[0];
-    showAccount.innerHTML = currentAccount;
+  // Handle user accounts and accountsChanged (per EIP-1193)
+  async function handleAccountsChanged(newAccounts) {
+    if (newAccounts.length === 0) {
+      console.log('Please connect to MetaMask.');
+    } else if (newAccounts[0] !== currentAccount) {
+      currentAccount = newAccounts[0];
+      showAccount.innerHTML = currentAccount;
+    }
   }
-}
 
-/*********************************************/
-/* Access the user's accounts (per EIP-1102) */
-/*********************************************/
+  let currentAccount = null;
 
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
+  ethereum.on('accountsChanged', handleAccountsChanged);
 
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
-async function getAccount() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((err) => {
+  // Enable Ethereum button click event
+  ethereumButton.addEventListener('click', async () => {
+    try {
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      currentAccount = accounts[0];
+      showAccount.innerHTML = currentAccount;
+    } catch (err) {
       if (err.code === 4001) {
         console.log('Please connect to MetaMask.');
       } else {
         console.error(err);
       }
-    });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
+    }
+  });
 }
